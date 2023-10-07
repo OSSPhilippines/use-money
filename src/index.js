278367cb0a4921c9e252a95ea2c1fc044d874a78
Joff Tiquez <jofftiquez@gmail.com>
2023-10-08 02:19:51 +0800

    chore: change bundler form vue-cli to vite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import Money from './components/Money';
+import Money from './components/Money.vue';
 import useMoney from './composables/use-money';
 
 export {
   Money,
   useMoney,
+};
+
+export default {
+  install (app, options) {
+    app.component('Money', Money);
+    app.config.globalProperties.$useMoney = useMoney;
+  }
 };
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'use-money',
+      fileName: (format) => `use-money.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  plugins: [vue()],
+})
